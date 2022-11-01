ab80a1364df11386131e8325a8a8e7767db66cf0
Stach Redeker <64889011+StachRedeker@users.noreply.github.com>
2022-11-01 18:22:07 +0100

    x

--- Stach/fibonacci v3/fibonacci.asm
+++ Stach/fibonacci v3/fibonacci.asm
@@ -1,19 +1,23 @@
-.begin
-.org 0
-
+.begin					
+.org 0						
+							
 ld [H], %r4               	! loads whether or not we want half of the result
-add %r0, %r0, %r1
-add %r0, 1, %r2
-add %r0, 1, %r6 			! permanent 1
+add %r2, 1, %r2
+add %r6, 1, %r6 			! %r6 will contain a permanent 1
 
 start: ld [C], %r5		! keeps track of the iterations
 addcc %r5, -1, %r5
 st %r5, [C]
 bneg ending
 
-add %r1, %r2, %r3         	! start of the Fibonacci calculation
-add %r0, %r2, %r1
-add %r0, %r3, %r2       		! %r2 will contain the result
+add %r2, %r3, %r3			! %r10 we use to make a backup of the value
+add %r1, %r3, %r3
+
+and %r0, %r1, %r1			! clear reg1
+add %r2, %r1, %r1
+
+and %r0, %r2, %r2			! clear reg2
+add %r3, %r2, %r2       		! %r2 will contain the result
 ba start
 
 ending: orcc %r4, %r0, %r0	! check if we want the full result
